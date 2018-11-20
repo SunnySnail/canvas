918e73da27316d0e826bdc74a4d667a34df563d8
Fasion Chan <fasionchan@gmail.com>
2018-11-20 09:34:30 +0800

    first version

---
+++ docs/zh_CN/scripts/auto-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# FileName:   auto-build.sh
+# Author:     Fasion Chan
+# @contact:   fasionchan@gmail.com
+# @version:   $Id$
+#
+# Description:
+#
+# Changelog:
+#
+#
+
+SELF_PATH=`realpath "$0"`
+SCRIPT_DIR_PATH=`dirname "$SELF_PATH"`
+DOC_ROOT_PATH=`dirname "$SCRIPT_DIR_PATH"`
+PYENV_PATH="$DOC_ROOT_PATH/opt/pyenv"
+BUILD_PATH="$DOC_ROOT_PATH/_build"
+
+BIND_ADDR=canvas.local
+sudo ifconfig lo0 alias $BIND_ADDR up
+
+rm -rf "$BUILD_PATH"
+PATH="$PYENV_PATH/bin:$PATH" "$PYENV_PATH/bin/sphinx-autobuild" -H $BIND_ADDR . "$BUILD_PATH/html"
